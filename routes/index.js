6baa865a4a7d1a03d65d799819b72cc1d6a9d621
Muhammad Abdullah <abdullahfast95@gmail.com>
2023-07-30 21:00:40 +1000

    Table Added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SIT722 DevOps Glossary' });
 });
 
 module.exports = router;
