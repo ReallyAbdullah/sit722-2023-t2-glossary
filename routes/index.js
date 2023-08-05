4f86aa42116128680373b4645b9f1f57ecf0fd59
Muhammad Abdullah <abdullahfast95@gmail.com>
2023-08-05 15:09:38 +1000

    fixed dynamic json data retrieval

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SIT722 DevOps Glossary' });
+  const data = require('./data.json');
+  console.log(data);
+  res.render('index', {myData :{ title: 'SIT722 DevOps Glossary', 'tableData': data.item }});
 });
 
 module.exports = router;
